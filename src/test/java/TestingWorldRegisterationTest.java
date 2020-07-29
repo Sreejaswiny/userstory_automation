85e9c74b89e3f489d29b0e67d5431e22f007789f
Sreejaswiny <sreekrish28@gmail.com>
2020-07-29 14:10:41 -0700

    commented out phpTravel feature and TestingworldRegisterationTest

--- src/test/java/TestingWorldRegisterationTest.java
+++ src/test/java/TestingWorldRegisterationTest.java
@@ -1,34 +1,35 @@
-import Pages.TestingWorldRegPage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static support1.TestContext.getDriver;
-import static support1.TestContext.initialize;
-
-public class TestingWorldRegisterationTest {
-
-    @Before
-    public void setup(){
-        initialize();
-        new TestingWorldRegPage().open();
-    }
-
-    @Test
-    public void registerationTest() throws InterruptedException {
-        new TestingWorldRegPage().fillRegisterationDetails();
-        new TestingWorldRegPage().selectHomeRadioButton();
-        new TestingWorldRegPage().selectGender();
-        new TestingWorldRegPage().selectCountry();
-        new TestingWorldRegPage().selectState();
-        new TestingWorldRegPage().selectCity();
-        new TestingWorldRegPage().zipcode();
-        new TestingWorldRegPage().selectCheckbox();
-        System.out.println("Testing world Page");
-    }
-
-    @After
-    public void tearDown(){
-        getDriver().quit();
-    }
-}
+//import Pages.TestingWorldRegPage;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static support.TestContext.getDriver;
+//import static support.TestContext.initialize;
+//
+//
+//public class TestingWorldRegisterationTest {
+//
+//    @Before
+//    public void setup(){
+//        initialize();
+//        new TestingWorldRegPage().open();
+//    }
+//
+//    @Test
+//    public void registerationTest() throws InterruptedException {
+//        new TestingWorldRegPage().fillRegisterationDetails();
+//        new TestingWorldRegPage().selectHomeRadioButton();
+//        new TestingWorldRegPage().selectGender();
+//        new TestingWorldRegPage().selectCountry();
+//        new TestingWorldRegPage().selectState();
+//        new TestingWorldRegPage().selectCity();
+//        new TestingWorldRegPage().zipcode();
+//        new TestingWorldRegPage().selectCheckbox();
+//        System.out.println("Testing world Page");
+//    }
+//
+//    @After
+//    public void tearDown(){
+//        getDriver().quit();
+//    }
+//}
